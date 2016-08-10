56a60993256a3f617546068cbb1d66f4e94270e2
Quinten De Swaef <quintendeswaef@gmail.com>
2016-08-10 11:22:57 +0200

    building timeseries from krakentrades

---
+++ core/src/test/java/com/cryptopurse/cryptotrader/market/domain/KrakenTradeFixture.java
@@ -0,0 +1,19 @@
+package com.cryptopurse.cryptotrader.market.domain;
+
+import org.knowm.xchange.dto.Order;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class KrakenTradeFixture {
+
+    public static KrakenTrade aTrade() {
+        return new KrakenTrade()
+                .setTime(new Date())
+                .setOrderType(Order.OrderType.ASK)
+                .setCurrencyPair("ETH/EUR")
+                .setAmount(new BigDecimal(10))
+                .setPrice(new BigDecimal(12));
+    }
+
+}
---
+++ core/src/test/java/com/cryptopurse/cryptotrader/market/timeseries/KrakenTimeSeriesBuilderTest.java
@@ -0,0 +1,40 @@
+package com.cryptopurse.cryptotrader.market.timeseries;
+
+import com.cryptopurse.cryptotrader.market.domain.KrakenTrade;
+import com.cryptopurse.cryptotrader.market.domain.KrakenTradeFixture;
+import eu.verdelhan.ta4j.Decimal;
+import eu.verdelhan.ta4j.TimeSeries;
+import org.joda.time.DateTime;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(MockitoJUnitRunner.class)
+public class KrakenTimeSeriesBuilderTest {
+
+    @InjectMocks
+    private KrakenTimeSeriesBuilder timeSeriesBuilder;
+
+    @Test
+    public void testBuildTimeseries() throws Exception {
+        KrakenTrade lastTrade = KrakenTradeFixture.aTrade();
+        KrakenTrade middleTrade = KrakenTradeFixture.aTrade().setTime(DateTime.now().minusMinutes(4).toDate())
+                .setPrice(new BigDecimal(11));
+        KrakenTrade firstTrade = KrakenTradeFixture.aTrade().setTime(DateTime.now().minusMinutes(10).toDate())
+                .setPrice(new BigDecimal(10));
+        TimeSeries timeseries = timeSeriesBuilder.timeseries(Arrays.asList(firstTrade, middleTrade, lastTrade));
+        assertThat(timeseries).isNotNull();
+
+        assertThat(timeseries.getTickCount()).isEqualTo(2);
+        assertThat(timeseries.getFirstTick().getAmount()).isEqualTo(Decimal.valueOf(firstTrade.getAmount().doubleValue()));
+
+        assertThat(timeseries.getLastTick().getOpenPrice()).isEqualTo(Decimal.valueOf(11));
+        assertThat(timeseries.getLastTick().getClosePrice()).isEqualTo(Decimal.valueOf(12));
+    }
+}

d5393b2bad14dc8e7e04cc4e555e40381d84ec46
Quinten De Swaef <quintendeswaef@gmail.com>
2016-08-11 23:16:58 +0200

    bigger columns for advice_period

--- core/src/main/resources/db/migration/V10__kraken_general_advice.sql
+++ core/src/main/resources/db/migration/V10__kraken_general_advice.sql
@@ -1,7 +1,7 @@
 create table kraken_general_advice(
     id bigint(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     currency_pair varchar(10) NOT NULL,
-    strategy_period varchar(10) NOT NULL,
+    strategy_period varchar(30) NOT NULL,
     strategy_type varchar(10) NOT NULL,
     strategy_advice varchar(10) NOT NULL,
     strategy_time timestamp not null,

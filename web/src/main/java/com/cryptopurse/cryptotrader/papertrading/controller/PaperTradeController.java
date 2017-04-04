50e6baec34d7dfa2a950d98992d0fd84b55b8636
Quinten De Swaef <quintendeswaef@gmail.com>
2017-04-04 16:17:19 +0200

    updated papertrades with some more information

--- web/src/main/java/com/cryptopurse/cryptotrader/papertrading/controller/PaperTradeController.java
+++ web/src/main/java/com/cryptopurse/cryptotrader/papertrading/controller/PaperTradeController.java
@@ -1,8 +1,22 @@
 package com.cryptopurse.cryptotrader.papertrading.controller;
 
+import com.cryptopurse.cryptotrader.advice.domain.StrategyPeriod;
+import com.cryptopurse.cryptotrader.exchange.service.supported.SupportedExchanges;
+import com.cryptopurse.cryptotrader.market.domain.TradeHistory;
+import com.cryptopurse.cryptotrader.market.service.TradehistoryService;
+import com.cryptopurse.cryptotrader.market.timeseries.TimeSeriesOrderLocator;
+import com.cryptopurse.cryptotrader.market.timeseries.TimeseriesBuilder;
 import com.cryptopurse.cryptotrader.papertrading.controller.dto.PaperTradePerConfigurationDTo;
 import com.cryptopurse.cryptotrader.papertrading.service.PaperTradeService;
 import com.cryptopurse.cryptotrader.usermanagement.utils.SecurityUtility;
+import eu.verdelhan.ta4j.Order;
+import eu.verdelhan.ta4j.TimeSeries;
+import eu.verdelhan.ta4j.TradingRecord;
+import eu.verdelhan.ta4j.analysis.criteria.AverageProfitCriterion;
+import eu.verdelhan.ta4j.analysis.criteria.AverageProfitableTradesCriterion;
+import eu.verdelhan.ta4j.analysis.criteria.LinearTransactionCostCriterion;
+import eu.verdelhan.ta4j.analysis.criteria.TotalProfitCriterion;
+import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -10,6 +24,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Comparator;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Controller
@@ -18,6 +33,13 @@ public class PaperTradeController {
 
     @Autowired
     private PaperTradeService paperTradeService;
+    @Autowired
+    private TradehistoryService tradehistoryService;
+    @Autowired
+    private TimeseriesBuilder timeseriesBuilder;
+    @Autowired
+    private TimeSeriesOrderLocator locator;
+
 
     @RequestMapping(method = RequestMethod.GET)
     public String index(final ModelMap map) {
@@ -28,12 +50,39 @@ public class PaperTradeController {
                     .entrySet()
                     .stream()
                     .map(entry -> new PaperTradePerConfigurationDTo(entry.getKey(), entry.getValue()))
+                    .map(config -> {
+                        final TimeSeries trades = getTrades(config.getConfiguration().getExchange(), config.getConfiguration().getStrategyPeriod());
+                        List<Order> orders = config.getTrades()
+                                .parallelStream()
+                                .map(order -> locator.convertToTradingRecord(trades, order))
+                                .collect(Collectors.toList());
+                        final TradingRecord tradingRecord = new TradingRecord(orders.toArray(new Order[orders.size()]));
+                        double totalProfit = new TotalProfitCriterion()
+                                .calculate(trades, tradingRecord);
+                        double transactionCosts = new LinearTransactionCostCriterion(1.00, 0.015)
+                                .calculate(trades, tradingRecord);
+                        double averageProfitableTrades = new AverageProfitableTradesCriterion()
+                                .calculate(trades, tradingRecord);
+                        double averageProfit = new AverageProfitCriterion()
+                                .calculate(trades, tradingRecord);
+                        return config
+                                .setAverageProfit(String.valueOf(averageProfit))
+                                .setAverageProfitableTrades(String.valueOf(averageProfitableTrades))
+                                .setTotalProfit(String.valueOf(totalProfit))
+                                .setTransactionCosts(String.valueOf(transactionCosts));
+
+                    })
                     .sorted(Comparator.comparingLong(p -> p.getConfiguration().getId()))
                     .collect(Collectors.toList()));
 
             return "user/papertrades";
         }
 
+    }
+
+    private TimeSeries getTrades(final SupportedExchanges exchange, StrategyPeriod period) {
+        List<TradeHistory> recentTrades = tradehistoryService.findRecentTrades(DateTime.now().minusDays(21).toDate(), exchange);
+        return timeseriesBuilder.timeseries(recentTrades, period.getTimeframeInSeconds());
     }
 
 }
--- web/src/main/java/com/cryptopurse/cryptotrader/papertrading/controller/dto/PaperTradePerConfigurationDTo.java
+++ web/src/main/java/com/cryptopurse/cryptotrader/papertrading/controller/dto/PaperTradePerConfigurationDTo.java
@@ -9,6 +9,10 @@ public class PaperTradePerConfigurationDTo {
 
     private PaperTradeConfiguration configuration;
     private List<PaperTrade> trades;
+    private String totalProfit;
+    private String averageProfitableTrades;
+    private String transactionCosts;
+    private String averageProfit;
 
     public PaperTradePerConfigurationDTo(final PaperTradeConfiguration configuration, final List<PaperTrade> trades) {
         this.configuration = configuration;
@@ -30,6 +34,42 @@ public class PaperTradePerConfigurationDTo {
 
     public PaperTradePerConfigurationDTo setTrades(final List<PaperTrade> trades) {
         this.trades = trades;
+        return this;
+    }
+
+    public String getTotalProfit() {
+        return totalProfit;
+    }
+
+    public PaperTradePerConfigurationDTo setTotalProfit(final String totalProfit) {
+        this.totalProfit = totalProfit;
+        return this;
+    }
+
+    public String getAverageProfitableTrades() {
+        return averageProfitableTrades;
+    }
+
+    public PaperTradePerConfigurationDTo setAverageProfitableTrades(final String averageProfitableTrades) {
+        this.averageProfitableTrades = averageProfitableTrades;
+        return this;
+    }
+
+    public String getTransactionCosts() {
+        return transactionCosts;
+    }
+
+    public PaperTradePerConfigurationDTo setTransactionCosts(final String transactionCosts) {
+        this.transactionCosts = transactionCosts;
+        return this;
+    }
+
+    public String getAverageProfit() {
+        return averageProfit;
+    }
+
+    public PaperTradePerConfigurationDTo setAverageProfit(final String averageProfit) {
+        this.averageProfit = averageProfit;
         return this;
     }
 }
